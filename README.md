36026b169f1b25a0a30ce8a3c8658cf7c22f7741
Kristopher Parrillo <kkparrillo@gmail.com>
2017-11-14 23:13:02 +0900

    updates README.md

---
+++ README.md
@@ -0,0 +1,8 @@
+## Revolution Prep Code Test
+
+### Technologies 
+- HTML5
+- CSS3 (Bootstrap 4)
+- jQuery
+- Animate.css
+
--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log('hello from main.js')
+const $container = $(".container");
+const $form = $("form");
+
+// funciton that creates thank you message after a successful form submission
+const formSuccess = event => {
+    event.preventDefault();
+    $form.remove();
+    const thankYouMessage = "<p class='mt-5 ml-3 animated fadeIn'>Thank you! We will send you more information via email shortly.</p>";
+    const button = "<a href='https://www.revolutionprep.com/' class='btn btn-primary mt-4 ml-3 pt-3' role='button'>Return Home</a>";
+    const html = thankYouMessage + button;
+    $container.append(html);
+}
+
+$form.on('submit', formSuccess);
